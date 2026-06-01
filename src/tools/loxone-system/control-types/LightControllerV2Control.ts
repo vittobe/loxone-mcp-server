914fc3e4ecbe9557fd928f7ad132d46a5ce8294c
Jeroen Van Daele <jeroen@MacBookPro2026.local>
2026-06-01 11:04:47 +0200

    fix: no URL encoding for subcontrol UUID, WebSocket uses plain slash

--- src/tools/loxone-system/control-types/LightControllerV2Control.ts
+++ src/tools/loxone-system/control-types/LightControllerV2Control.ts
@@ -115,10 +115,8 @@ export class LightControllerV2Control extends AbstractControlType {
       if (colonIdx === -1) throw new Error('setLight value must be in format "subControlUuid:value"');
       const subUuid = str.substring(0, colonIdx);
       const subValue = str.substring(colonIdx + 1);
-      // Loxone verwacht de / in subcontrol UUIDs als %2F in de HTTP URL
-      const encodedSubUuid = subUuid.replace('/', '%2F');
-      // on, off of een dimwaarde (0-100) werken allemaal direct
-      return `jdev/sps/io/${encodedSubUuid}/${subValue}`;
+      // Via WebSocket (intern gebruikt door loxone-ts-api) gewoon / gebruiken, geen %2F
+      return `jdev/sps/io/${subUuid}/${subValue}`;
     } else if (command === 'hsv' || command === 'temp' || command === 'setBrightness') {
       // Forward color commands to ColorPickerV2 subcontrol
       let colorPickerUuid: string | undefined;
